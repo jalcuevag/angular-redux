a2688cf2408cf4926f7b049833d1ce7288c76616
jalcuevag <jalcuevag@gmail.com>
2021-04-22 01:11:22 -0500

    Angular without redux

---
+++ src/app/contador/nieto/nieto.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input, EventEmitter, OnInit, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-nieto',
+  templateUrl: './nieto.component.html',
+  styleUrls: ['./nieto.component.css'],
+})
+export class NietoComponent implements OnInit {
+  @Input() contador: number;
+  @Output() contadorCambio = new EventEmitter<number>();
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  reset() {
+    this.contador = 0;
+    this.contadorCambio.emit(0);
+  }
+}
